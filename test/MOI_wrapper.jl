d36b81078eef59a8199abf397d875f59285d96a0
Benoît Legat <benoit.legat@gmail.com>
2019-02-08 02:36:52 +0100

    Simplify thanks to NonposToNonnegBridge

--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -3,42 +3,41 @@ using Test
 using MathOptInterface
 const MOI = MathOptInterface
 const MOIT = MOI.Test
-const MOIB = MOI.Bridges
-
-using CDCS
-
 const MOIU = MOI.Utilities
-MOIU.@model(ModelData,
-            (),
-            (),
-            (MOI.Zeros, MOI.Nonnegatives, MOI.Nonpositives, MOI.SecondOrderCone,
-             MOI.RotatedSecondOrderCone, MOI.PositiveSemidefiniteConeTriangle),
-            (),
-            (),
-            (),
-            (MOI.VectorOfVariables,),
-            (MOI.VectorAffineFunction,))
+const MOIB = MOI.Bridges
 
 # Iterations:
 # linear5 : > 1000, < 2000
 # linear9 : > 3000, < 4000
 # linear15: > 20000, I don't know if ever converges so we exclude it
-optimizer = MOIU.CachingOptimizer(MOIU.UniversalFallback{Float64}(ModelData{Float64}()),
-                                  CDCS.Optimizer(verbose=0, maxIter=4000))
+import CDCS
+optimizer = CDCS.Optimizer(verbose=0, maxIter=4000)
 
 @testset "SolverName" begin
     @test MOI.get(optimizer, MOI.SolverName()) == "CDCS"
 end
 
 @testset "supports_allocate_load" begin
-    @test MOIU.supports_allocate_load(optimizer.optimizer, false)
-    @test !MOIU.supports_allocate_load(optimizer.optimizer, true)
+    @test MOIU.supports_allocate_load(optimizer, false)
+    @test !MOIU.supports_allocate_load(optimizer, true)
 end
+
+MOIU.@model(ModelData, (), (),
+            (MOI.Zeros, MOI.Nonnegatives, MOI.SecondOrderCone,
+             MOI.RotatedSecondOrderCone, MOI.PositiveSemidefiniteConeTriangle),
+            (), (), (), (MOI.VectorOfVariables,), (MOI.VectorAffineFunction,))
+
+# UniversalFallback is needed for starting values, even if they are ignored by CDCS
+const cache = MOIU.UniversalFallback(ModelData{Float64}())
+const cached = MOIU.CachingOptimizer(cache, optimizer)
+
+# Essential bridges that are needed for all tests
+const bridged = MOIB.Vectorize{Float64}(MOIB.NonposToNonneg{Float64}(cached))
 
 config = MOIT.TestConfig(atol=3e-2, rtol=3e-2)
 
 @testset "Unit" begin
-    MOIT.unittest(MOIB.SplitInterval{Float64}(MOIB.Vectorize{Float64}(optimizer)),
+    MOIT.unittest(MOIB.SplitInterval{Float64}(bridged),
                   config,
                   [# Need to investigate...
                    "solve_with_lowerbound", "solve_affine_deletion_edge_cases", "solve_blank_obj",
@@ -49,12 +48,12 @@ config = MOIT.TestConfig(atol=3e-2, rtol=3e-2)
 end
 
 @testset "Continuous linear problems" begin
-    MOIT.contlineartest(MOIB.SplitInterval{Float64}(MOIB.Vectorize{Float64}(optimizer)),
+    MOIT.contlineartest(MOIB.SplitInterval{Float64}(bridged),
                         config, ["linear12", "linear15"])
 end
 
 @testset "Continuous conic problems" begin
-    MOIT.contconictest(MOIB.SquarePSD{Float64}(MOIB.RootDet{Float64}(MOIB.GeoMean{Float64}(MOIB.RSOC{Float64}(MOIB.Vectorize{Float64}(optimizer))))),
+    MOIT.contconictest(MOIB.SquarePSD{Float64}(MOIB.RootDet{Float64}(MOIB.GeoMean{Float64}(MOIB.RSOC{Float64}(bridged)))),
                        config, [# See https://github.com/JuliaOpt/MathOptInterface.jl/pull/632,
                                 "rotatedsoc1v",
                                 "rotatedsoc2", "rootdets", "exp", "logdet"])
