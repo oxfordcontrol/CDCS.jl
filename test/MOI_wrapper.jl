d22622e9e5af3e0394a53b1e9d7a75f67bbefb6a
Benoît Legat <benoit.legat@gmail.com>
2022-03-13 11:23:20 -0400

    Add JuliaFormatter

--- test/MOI_wrapper.jl
+++ test/MOI_wrapper.jl
@@ -11,7 +11,11 @@ const MOIB = MOI.Bridges
 # linear9 : > 3000, < 4000
 # linear15: > 20000, Don't know if ever converges so we exclude it
 import CDCS
-const OPTIMIZER_CONSTRUCTOR = MOI.OptimizerWithAttributes(CDCS.Optimizer, MOI.Silent() => true, "maxIter" => 4000)
+const OPTIMIZER_CONSTRUCTOR = MOI.OptimizerWithAttributes(
+    CDCS.Optimizer,
+    MOI.Silent() => true,
+    "maxIter" => 4000,
+)
 const OPTIMIZER = MOI.instantiate(OPTIMIZER_CONSTRUCTOR)
 
 @testset "SolverName" begin
@@ -23,43 +27,71 @@ end
     @test !MOIU.supports_allocate_load(OPTIMIZER, true)
 end
 
-const BRIDGED = MOI.instantiate(OPTIMIZER_CONSTRUCTOR, with_bridge_type=Float64)
-const CONFIG = MOIT.TestConfig(atol=3e-2, rtol=3e-2)
+const BRIDGED =
+    MOI.instantiate(OPTIMIZER_CONSTRUCTOR, with_bridge_type = Float64)
+const CONFIG = MOIT.TestConfig(atol = 3e-2, rtol = 3e-2)
 
 @testset "Unit" begin
-    MOIT.unittest(BRIDGED, CONFIG, [
-        # `NumberOfThreads` not supported.
-        "number_threads",
-        # `TimeLimitSec` not supported.
-        "time_limit_sec",
-        # Need to investigate...
-        "solve_with_lowerbound", "solve_affine_deletion_edge_cases", "solve_blank_obj", "solve_single_variable_dual_min", "solve_single_variable_dual_max",
-        # Need https://github.com/JuliaOpt/MathOptInterface.jl/issues/529
-        "solve_qp_edge_cases",
-        # Error using cdcs_hsde.preprocess (line 14)
-        # No variables in your problem?
-        "solve_unbounded_model",
-        # Integer and ZeroOne sets are not supported
-        "solve_integer_edge_cases", "solve_objbound_edge_cases",
-        "solve_zero_one_with_bounds_1",
-        "solve_zero_one_with_bounds_2",
-        "solve_zero_one_with_bounds_3"])
+    MOIT.unittest(
+        BRIDGED,
+        CONFIG,
+        [
+            # `NumberOfThreads` not supported.
+            "number_threads",
+            # `TimeLimitSec` not supported.
+            "time_limit_sec",
+            # Need to investigate...
+            "solve_with_lowerbound",
+            "solve_affine_deletion_edge_cases",
+            "solve_blank_obj",
+            "solve_single_variable_dual_min",
+            "solve_single_variable_dual_max",
+            # Need https://github.com/JuliaOpt/MathOptInterface.jl/issues/529
+            "solve_qp_edge_cases",
+            # Error using cdcs_hsde.preprocess (line 14)
+            # No variables in your problem?
+            "solve_unbounded_model",
+            # Integer and ZeroOne sets are not supported
+            "solve_integer_edge_cases",
+            "solve_objbound_edge_cases",
+            "solve_zero_one_with_bounds_1",
+            "solve_zero_one_with_bounds_2",
+            "solve_zero_one_with_bounds_3",
+        ],
+    )
 end
 
 @testset "Continuous linear problems" begin
     MOIT.contlineartest(BRIDGED, CONFIG, [
         # Need to investigate...
-        "linear12", "linear15"])
+        "linear12",
+        "linear15",
+    ])
 end
 
 @testset "Continuous conic problems" begin
-    MOIT.contconictest(BRIDGED, CONFIG, [
-        # ITERATION_LIMIT
-        "normone1v", "normone1f",
-        # rotatedsoc2: Returns Inf and -Inf instead of infeasibility certificate
-        "rotatedsoc2",
-        # Need to investigate...
-        "psdt3", "psds3",
-        # Unsupported cones
-        "pow", "dualpow", "rootdets", "exp", "dualexp", "logdet", "normspec", "normnuc", "relentr"])
+    MOIT.contconictest(
+        BRIDGED,
+        CONFIG,
+        [
+            # ITERATION_LIMIT
+            "normone1v",
+            "normone1f",
+            # rotatedsoc2: Returns Inf and -Inf instead of infeasibility certificate
+            "rotatedsoc2",
+            # Need to investigate...
+            "psdt3",
+            "psds3",
+            # Unsupported cones
+            "pow",
+            "dualpow",
+            "rootdets",
+            "exp",
+            "dualexp",
+            "logdet",
+            "normspec",
+            "normnuc",
+            "relentr",
+        ],
+    )
 end
--- test/sdp.jl
+++ test/sdp.jl
@@ -15,24 +15,28 @@ using CDCS
         # We add y ≥ 0 to make it bounded, Now the optimal solution is `(2, 0)`
         # with the matrix [1 1
         #                  1 1] of eigenvalues (0, 2).
-        A = -[0.0 0.0 0.0 1.0 0.0
-              1.0 0.0 1.0 0.0 0.0]
+        A = -[
+            0.0 0.0 0.0 1.0 0.0
+            1.0 0.0 1.0 0.0 0.0
+        ]
         b = [1.0, 0.0]
         c = [0.0, 1.0, 0.0, 0.0, 1.0]
-        primal, dual, z, info = cdcs(Matrix(A'), b, c, CDCS.Cone(0, 1, [], [2]),
-                                     verbose=0)
-        @test primal ≈ [1, 1, -1, -1, 1] atol=1e-3
-        @test dual ≈ [2.0, 0.0] atol=1e-3
-        @test z ≈ [0, 1, 1, 1, 1] atol=1e-3
+        primal, dual, z, info =
+            cdcs(Matrix(A'), b, c, CDCS.Cone(0, 1, [], [2]), verbose = 0)
+        @test primal ≈ [1, 1, -1, -1, 1] atol = 1e-3
+        @test dual ≈ [2.0, 0.0] atol = 1e-3
+        @test z ≈ [0, 1, 1, 1, 1] atol = 1e-3
         @test info["problem"] == 0.0
     end
     @testset "Unbounded" begin
-        A = -[0.0 0.0 1.0 0.0
-              0.0 1.0 0.0 0.0]
+        A = -[
+            0.0 0.0 1.0 0.0
+            0.0 1.0 0.0 0.0
+        ]
         b = [1.0, 0.0]
         c = [1.0, 0.0, 0.0, 1.0]
-        primal, dual, z, info = cdcs(Matrix(A'), b, c, CDCS.Cone(0, 0, [], [2]),
-                                     verbose=0)
+        primal, dual, z, info =
+            cdcs(Matrix(A'), b, c, CDCS.Cone(0, 0, [], [2]), verbose = 0)
         @test primal == [Inf, -Inf, -Inf, Inf]
         @test dual == [Inf, -Inf]
         @test z == [Inf, -Inf, -Inf, Inf]
